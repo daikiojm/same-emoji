1e2a4155ebd5d44a2acf1603a9ac133a49f4dbdd
daikiojm <daiking.ca2@gmail.com>
2018-06-10 16:29:49 +0900

    implement start-up page

--- src/app/pages/start-up-page/start-up-page.component.ts
+++ src/app/pages/start-up-page/start-up-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'se-start-up-page',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-up-page.component.scss'],
 })
 export class StartUpPageComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  onClickStart(): void {
+    this.router.navigate(['/play']);
+  }
 }
