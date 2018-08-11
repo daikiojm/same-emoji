6cdb502f5ee9a2c3de7f027ed08ea4d6bff4cf2f
Daiki Ojima <daiking.ca2@gmail.com>
2018-08-11 15:55:08 +0900

    feat(level): add level select page template (#8)
    * feat(level): add level select page template
    
    * chore(level): read level select buttons from constants

--- src/app/pages/game-level-page/game-level-page.component.ts
+++ src/app/pages/game-level-page/game-level-page.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { gameLevels } from '../../constants';
+
 @Component({
   selector: 'se-game-level-page',
   templateUrl: './game-level-page.component.html',
   styleUrls: ['./game-level-page.component.scss'],
 })
 export class GameLevelPageComponent implements OnInit {
+  gameLevels = gameLevels;
+
   constructor() {}
 
   ngOnInit() {}
